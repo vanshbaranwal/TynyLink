85081e821fcdfb8ceec19bd9c20c62ccdd89eeba
Vansh Baranwal <vanshveerbaranwal@gmail.com>
2026-08-27 20:25:10 +0530

    initialing the frontend

---
+++ FRONTEND/src/components/UrlForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const UrlForm = () => {
+  return (
+    <form className='space-y-4'>
+        <div>
+            <label htmlFor="url" className='block text-sm font medium text-gray'></label>
+        </div>
+    </form>
+  )
+}
+
+
+export default UrlForm
+
